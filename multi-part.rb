3e7d95472d63994ae5c76293afa3916ce51f826b
Jumer Acosta <jumeracosta@deltek.com>
2017-07-03 18:18:04 +0800

    adding file upload script

---
+++ multi-part.rb
@@ -0,0 +1,33 @@
+require 'uri'
+require 'net/http'
+require 'yaml'
+
+
+user = YAML.load_file(File.dirname(__FILE__) + "/users_example.yml")
+auth = user[1]["auth_token"]
+
+require 'uri'
+require 'net/http'
+
+url = URI("http://localhost:3000/projects/8/attachments.json?auth_token=SeedT0K3N1")
+
+http = Net::HTTP.new(url.host, url.port)
+
+request = Net::HTTP::Post.new(url)
+request["content-type"] = 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+request["cache-control"] = 'no-cache'
+request["postman-token"] = '269a4916-cd17-e910-1342-ef2bb9e085a3'
+request.body = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"attachment[attachment]\"; filename=\"/files/test_pdf.pdf\"\r\nContent-Type: image/jpeg\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+
+response = http.request(request)
+puts response.read_body
+puts response.code
+
+# Typhoeus.post(
+#   "http://localhost:3000/posts",
+#   body: {
+#     title: "test post",
+#     content: "this is my test",
+#     file: File.open("thesis.txt","r")
+#   }
+# )
