d5e68cbd7d7b175ff5ab6b660e374b579d47e3e7
Jumer Acosta <jumeracosta@deltek.com>
2017-07-10 23:04:56 +0800

    Update multi_part.rb

--- multi_part.rb
+++ multi_part.rb
@@ -4,24 +4,24 @@ require 'net/http'
 require 'yaml'
 require 'openssl'
 
+#for authentication
+user = YAML.load_file(File.dirname(__FILE__) + "/users_example.yml")
+auth = user[2]["auth_token"]
+#url = URI("http://localhost:3000/projects/8/attachments.json?auth_token=#{auth}")
+url = URI("https://test.kona.com/projects/8/attachments.json?auth_token=#{auth}")
+
 files = Dir.glob('*/*').select {|f| File.file? f}
 puts "Processing all.. "
 
 $i = 1
-$no_execute = 2 #update to how many times it will execute
+$no_execute = 1 #given times it will execute
 
 while $i <= $no_execute do
 	files.each do |fname| #iterate over each file
 	 	file = File.open(fname)
 		byte_representation = file.read
-		#for authentication
-		user = YAML.load_file(File.dirname(__FILE__) + "/users_example.yml") 
-		auth = user[2]["auth_token"]
-		#url = URI("http://localhost:3000/projects/8/attachments.json?auth_token=#{auth}")
-		url = URI("https://test.kona.com/projects/8/attachments.json?auth_token=#{auth}")
 
 		http = Net::HTTP.new(url.host, url.port)
-
 		#check if https request
 		if url.instance_of? URI::HTTPS
 			http.use_ssl = true
