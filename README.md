167a700e4374f53738c6053d12372f3ca45b1c94
Jumer Acosta <jumeracosta@deltek.com>
2017-07-10 18:33:24 +0800

    updated fileUpload script

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-Recently, I've added 3 different file types(.txt, .pdf and .jpg) under `files` folder.
+Recently, I've added 3 different file types(.txt, .pdf and .jpg) under files folder.
+
 To upload new file:
-  - add a new file in `files` folder
-  - then change the filename `filename=\"/files/test_pdf.pdf` in the `multi-part.rb` script
-  
-**Note: For now it's a single file upload.
+
+add new files in files folder
+then run ruby multi_part.rb
+**Note: It will upload all files in a directory.
--- multi_part.rb
+++ multi_part.rb
@@ -1,28 +1,37 @@
 require 'uri'
 require 'net/http'
 require 'yaml'
+require 'openssl'
 
 files = Dir.glob('*/*').select {|f| File.file? f}
 puts "Processing all.. "
 
 files.each do |fname| #iterate over each file
-	file = File.open(fname)
+ 	file = File.open(fname)
 	byte_representation = file.read
-
-	user = YAML.load_file(File.dirname(__FILE__) + "/users_example.yml") #for authentication
-	auth = user[1]["auth_token"]
-	url = URI("http://localhost:3000/projects/8/attachments.json?auth_token=#{auth}")
+	#for authentication
+	user = YAML.load_file(File.dirname(__FILE__) + "/users_example.yml") 
+	auth = user[2]["auth_token"]
+	#url = URI("http://localhost:3000/projects/8/attachments.json?auth_token=#{auth}")
+	url = URI("https://test.kona.com/projects/8/attachments.json?auth_token=#{auth}")
 
 	http = Net::HTTP.new(url.host, url.port)
 
+	#check if https request
+	if url.instance_of? URI::HTTPS
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+	end
+
+	#POST request to upload all files in a directory
 	request = Net::HTTP::Post.new(url)
 	request["content-type"] = 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
-	request["cache-control"] = 'no-cache'
-	request["postman-token"] = '928ab48e-9553-49c0-c651-7e6577b19e6c'
 	request.body = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"attachment[attachment]\"; filename=\"#{fname}\"\r\nContent-Type: image/jpeg\r\n\r\n#{byte_representation}\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
 			
 	response = http.request(request)
-	puts "#{fname}..." + response.code
-end
+	split_name = fname.split("/")[-1]
+	puts "#{split_name}..." + response.code
 
-puts "All files uploaded!.."
+
+end
+	#puts "All files uploaded!.."
